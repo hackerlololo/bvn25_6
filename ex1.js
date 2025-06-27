def7d07e2228b905f0b75c1d68d42de9e8741c69
hackerlololo <tomdavid000@gmail.com>
2025-06-27 18:49:49 +0700

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,21 @@
+function hiepML() {
+    alert("hiepml hoc IT");
+}
+
+
+let a=8;
+let b=3;
+console.log("Tổng:",a+b);
+console.log("Hieu:",a-b);
+console.log("Thuong:",a/b);
+console.log("Chia lay du:",a%b);
+console.log("Tổng:",a+b);
+
+console.log("a==b",a==b);
+console.log("a===b",a===b);
+console.log("a!=b",a!=b);
+console.log("a!==b",a!==b);
+console.log("a<b",a<b);
+console.log("a>b",a>b);
+console.log("a<=b",a<=b);
+console.log("a>=b",a>=b);
